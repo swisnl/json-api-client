ad9458f893396dc44c28a745308a3d9f199694ff
Jasper Zonneveld <jasper@swis.nl>
2018-02-09 15:56:00 +0100

    Updated readme with getting started and configuration

--- config/jsonapi.php
+++ config/jsonapi.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Base URI
+    |--------------------------------------------------------------------------
+    |
+    | Specify a base uri which will be prepended to every URI.
+    |
+    | Default: empty string
+    |
+    */
     'base_uri' => '',
 ];
