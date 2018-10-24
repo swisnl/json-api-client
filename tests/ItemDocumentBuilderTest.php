808d989d72b55fd1a45b28b430c02cef2a8b1d4d
Jasper Zonneveld <jasper@swis.nl>
2018-10-24 12:17:29 +0200

    Allow '0' as id in ItemDocumentBuilder

--- tests/ItemDocumentBuilderTest.php
+++ tests/ItemDocumentBuilderTest.php
@@ -20,11 +20,59 @@ class ItemDocumentBuilderTest extends AbstractTest
         $itemHydrator = new ItemHydrator($typeMapper);
         $itemDocumentBuilder = new ItemDocumentBuilder($itemHydrator);
 
-        $itemDocument = $itemDocumentBuilder->build(new Item(), $data, 123);
+        $itemDocument = $itemDocumentBuilder->build(new Item(), $data, '123');
 
         $item = $itemDocument->getData();
         static::assertInstanceOf(Item::class, $item);
+        static::assertEquals($item->getId(), '123');
         static::assertEquals($item->key1, $data['key1']);
         static::assertEquals($item->key2, $data['key2']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_fills_items_without_id()
+    {
+        $typeMapper = new TypeMapper();
+        $itemHydrator = new ItemHydrator($typeMapper);
+        $itemDocumentBuilder = new ItemDocumentBuilder($itemHydrator);
+
+        $itemDocument = $itemDocumentBuilder->build(new Item(), []);
+
+        $item = $itemDocument->getData();
+        static::assertInstanceOf(Item::class, $item);
+        static::assertNull($item->getId());
+    }
+
+    /**
+     * @test
+     * @dataProvider provideIdArguments
+     *
+     * @param $givenId
+     * @param $expectedId
+     */
+    public function it_fills_the_id_when_not_null_or_empty_string($givenId, $expectedId)
+    {
+        $typeMapper = new TypeMapper();
+        $itemHydrator = new ItemHydrator($typeMapper);
+        $itemDocumentBuilder = new ItemDocumentBuilder($itemHydrator);
+
+        $itemDocument = $itemDocumentBuilder->build(new Item(), [], $givenId);
+
+        $item = $itemDocument->getData();
+        static::assertInstanceOf(Item::class, $item);
+        static::assertSame($item->getId(), $expectedId);
+    }
+
+    public function provideIdArguments(): array
+    {
+        return [
+            ['0', '0'],
+            ['12', '12'],
+            ['foo-bar', 'foo-bar'],
+            [null, null],
+            ['', null],
+        ];
     }
 }
