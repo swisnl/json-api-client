603ca4fd0d9230e63931dbaaab49d4226fa9c97e
Hendrik Nijmeijer <hendrik@hendriknijmeijer.nl>
2025-05-28 10:01:06 +0200

    Fix type tests

--- tests/_mocks/MockRepository.php
+++ tests/_mocks/MockRepository.php
@@ -7,7 +7,7 @@ namespace Swis\JsonApi\Client\Tests\Mocks;
 use Swis\JsonApi\Client\Repository;
 
 /**
- * @implements \Swis\JsonApi\Client\Interfaces\RepositoryInterface<\Swis\JsonApi\Client\Tests\Mocks\ItemStub>
+ * @extends Repository<\Swis\JsonApi\Client\Tests\Mocks\ItemStub>
  */
 class MockRepository extends Repository
 {
--- types/Item.php
+++ types/Item.php
@@ -7,7 +7,7 @@ use function PHPStan\Testing\assertType;
 $item = new ParentItem;
 
 assertType('Swis\JsonApi\Client\Tests\Mocks\Items\ChildItem|null', $item->child()->getData());
-assertType('Swis\JsonApi\Client\Collection<(int|string), Swis\JsonApi\Client\Tests\Mocks\Items\ChildItem>|null', $item->children()->getData());
+assertType('Swis\JsonApi\Client\Collection<int, Swis\JsonApi\Client\Tests\Mocks\Items\ChildItem>|null', $item->children()->getData());
 
 assertType('Swis\JsonApi\Client\Interfaces\ItemInterface|null', $item->morph()->getData());
-assertType('Swis\JsonApi\Client\Collection<(int|string), Swis\JsonApi\Client\Interfaces\ItemInterface>|null', $item->morphmany()->getData());
+assertType('Swis\JsonApi\Client\Collection<int, Swis\JsonApi\Client\Interfaces\ItemInterface>|null', $item->morphmany()->getData());
